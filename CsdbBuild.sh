5a92311dda03d417180e61e14a6a39cdf67d7731
summerxwu <wuxia900717@gmail.com>
2022-03-30 17:06:39 +0800

    modify csbuild

--- CsdbBuild.sh
+++ CsdbBuild.sh
@@ -9,3 +9,4 @@ syspath="/usr/include"
 find ${path} -name "*.h*"  | xargs dirname  | grep -v include   |sed -e 's/^/-I/' | sort | uniq > .clang_complete
 echo "-I/data1/home/summerxwu/bustub/src/include" >> .clang_complete
 #find ${syspath} -name "*.h"  | xargs dirname  |sed -e 's/^/-I/' | sort | uniq >> .clang_complete
+
